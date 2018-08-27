9879ddddd7832d1ae456f8caaa12bc75b7aa112b
sudshanb <sudshanb@IN-MUM-LNDVM042.corp.capgemini.com>
2018-08-27 08:27:20 -0400

    File

--- src/com/sudarshan/day4/Manager.java
+++ src/com/sudarshan/day4/Manager.java
@@ -2,9 +2,15 @@ package com.sudarshan.day4;
 
 public class Manager extends Employee {
 	
+	String[] array= new String[] {"sudarshan", "shanbhag", "sgdha"};
+	
+	
 	public Manager() {
 	}
 	
+	public void sortStrings() {
+		
+	}
 	
 	
 }
